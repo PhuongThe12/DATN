32b4ee2911e9e51627ee7ad5ebc61772dcd18413
Quan Minh <quanvdmph19717@fpt.edu.vn>
2023-11-07 13:17:13 +0700

    update

--- src/main/java/luckystore/datn/service/impl/HoaDonChiTietServiceImpl.java
+++ src/main/java/luckystore/datn/service/impl/HoaDonChiTietServiceImpl.java
@@ -1,6 +1,8 @@
 package luckystore.datn.service.impl;
 
+import luckystore.datn.model.response.HoaDonChiTietResponse;
 import luckystore.datn.model.response.HoaDonResponse;
+import luckystore.datn.service.HoaDonChiTietService;
 import luckystore.datn.service.HoaDonService;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -8,19 +10,20 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class HoaDonChiTietServiceImpl implements HoaDonService {
+public class HoaDonChiTietServiceImpl implements HoaDonChiTietService {
+
     @Override
-    public List<HoaDonResponse> getAll() {
+    public List<HoaDonChiTietResponse> getAll() {
         return null;
     }
 
     @Override
-    public Page<HoaDonResponse> getPage(int page, String searchText, Integer status) {
+    public Page<HoaDonChiTietResponse> getPage(int page, String searchText, Integer status) {
         return null;
     }
 
     @Override
-    public HoaDonResponse findById(Long id) {
+    public HoaDonChiTietResponse findById(Long id) {
         return null;
     }
 }
