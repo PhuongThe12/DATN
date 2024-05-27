f054a6aae8b6923ddf87cdea1221a1d2e9ffabfa
Hoangnt95 <badboyy.nb@gmail.com>
2024-05-27 09:28:52 +0700

    update App

--- src/test/java/luckystore/datn/DatnApplicationTests.java
+++ src/test/java/luckystore/datn/DatnApplicationTests.java
@@ -3,11 +3,10 @@ package luckystore.datn;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(classes = DatnApplication.class)
 class DatnApplicationTests {
-
     @Test
-    void contextLoads() {
+    public void contextLoads() {
     }
-
 }
